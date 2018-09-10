1bfa8d512d6312afd31093eac9fdf2633fa66fb4
holmgr <viktor.holmgren@gmail.com>
2018-09-10 16:51:40 +0200

    Add ggez with basic drawing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
-fn main() {
-    println!("Hello, world!");
+extern crate ggez;
+
+use ggez::conf;
+use ggez::event;
+use ggez::graphics;
+use ggez::{Context, GameResult};
+use std::env;
+use std::path;
+
+struct MainState {
+    canvas: graphics::Canvas,
+    frames: usize,
+}
+
+impl MainState {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        // The ttf file will be in your resources directory. Later, we
+        // will mount that directory so we can omit it in the path here.
+        let canvas = graphics::Canvas::with_window_size(ctx)?;
+
+        let s = MainState { canvas, frames: 0 };
+        Ok(s)
+    }
+}
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::set_background_color(ctx, graphics::Color::from((216, 218, 223)));
+        graphics::clear(ctx);
+
+        graphics::present(ctx);
+        self.frames += 1;
+        if (self.frames % 100) == 0 {
+            println!("FPS: {}", ggez::timer::get_fps(ctx));
+        }
+
+        Ok(())
+    }
+}
+
+pub fn main() {
+    let c = conf::Conf {
+        window_setup: conf::WindowSetup {
+            samples: conf::NumSamples::Two,
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+    let ctx = &mut Context::load_from_conf("hansa", "ggez", c).unwrap();
+
+    let state = &mut MainState::new(ctx).unwrap();
+    if let Err(e) = event::run(ctx, state) {
+        println!("Error encountered: {}", e);
+    } else {
+        println!("Game exited cleanly.");
+    }
 }
