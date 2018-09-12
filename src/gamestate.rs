0ce8fcf112d6ad12395b39093e70495033e800c5
holmgr <viktor.holmgren@gmail.com>
2018-09-12 12:16:26 +0200

    Create empty basic world struct

--- src/gamestate.rs
+++ src/gamestate.rs
@@ -2,15 +2,20 @@ use ggez::{
     event, graphics, timer, {Context, GameResult},
 };
 
+use world::World;
+
 /// Handles and holds all game information.
 pub enum GameState {
-    Playing { frames: usize },
+    Playing { frames: usize, world: World },
 }
 
 impl GameState {
     /// Creates a new game state in Play mode.
     pub fn new(_ctx: &mut Context) -> GameResult<Self> {
-        let state = GameState::Playing { frames: 0 };
+        let state = GameState::Playing {
+            frames: 0,
+            world: World::default(),
+        };
         Ok(state)
     }
 }
@@ -24,9 +29,9 @@ impl event::EventHandler for GameState {
     /// Draws the current state to the screen with the given context.
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         match self {
-            GameState::Playing { frames } => {
-                graphics::set_background_color(ctx, graphics::Color::from((216, 218, 223)));
+            GameState::Playing { frames, world } => {
                 graphics::clear(ctx);
+                graphics::set_background_color(ctx, graphics::Color::from((216, 218, 223)));
 
                 graphics::present(ctx);
                 *frames += 1;
---
+++ src/world.rs
@@ -0,0 +1,19 @@
+
+/// Holds all information on the game world.
+pub struct World {}
+
+impl World {
+
+    /// Creates a new world.
+    pub fn new() -> Self {
+        World {}
+    }
+}
+
+impl Default for World {
+
+    /// Creates a default world.
+    fn default() -> Self {
+        World {}
+    }
+}
