71b61c3de6b44eff3bc83c61921e5231bd32633a
holmgr <viktor.holmgren@gmail.com>
2018-09-13 09:02:48 +0200

    Add skeleton for spritebatch drawing

--- src/gamestate.rs
+++ src/gamestate.rs
@@ -4,16 +4,27 @@ use ggez::{
 
 use world::World;
 
+const TILESET_PATH: &str = "/tileset.png";
+
 /// Handles and holds all game information.
 pub enum GameState {
-    Playing { frames: usize, world: World },
+    Playing {
+        frames: usize,
+        batch: graphics::spritebatch::SpriteBatch,
+        world: World,
+    },
 }
 
 impl GameState {
     /// Creates a new game state in Play mode.
-    pub fn new(_ctx: &mut Context) -> GameResult<Self> {
+    pub fn new(ctx: &mut Context) -> GameResult<Self> {
+        // Load spritebatch for effective drawing of sprites.
+        let image = graphics::Image::new(ctx, TILESET_PATH)?;
+        let batch = graphics::spritebatch::SpriteBatch::new(image);
+
         let state = GameState::Playing {
             frames: 0,
+            batch,
             world: World::default(),
         };
         Ok(state)
@@ -29,9 +40,22 @@ impl event::EventHandler for GameState {
     /// Draws the current state to the screen with the given context.
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         match self {
-            GameState::Playing { frames, world } => {
+            GameState::Playing {
+                frames,
+                world,
+                batch,
+            } => {
                 graphics::clear(ctx);
                 graphics::set_background_color(ctx, graphics::Color::from((216, 218, 223)));
+
+                // TODO: Draw something more useful :P
+                let param = graphics::DrawParam {
+                    dest: graphics::Point2::new(0.0, 0.0),
+                    ..Default::default()
+                };
+                batch.add(param);
+                graphics::draw_ex(ctx, batch, param)?;
+                batch.clear();
 
                 graphics::present(ctx);
                 *frames += 1;
