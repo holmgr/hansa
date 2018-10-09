e8a872d71ef2cae3dbad6d41e95e3bb0fe43d773
holmgr <viktor.holmgren@gmail.com>
2018-10-09 21:30:49 +0200

    Update colors to be less clashing

--- src/color.rs
+++ src/color.rs
@@ -10,9 +10,9 @@ impl Color {
     /// Return a tuple of the color's components in the RGB scheme.
     pub fn rgb(self) -> (u8, u8, u8) {
         match self {
-            Color::Blue => (72, 133, 237),
-            Color::Green => (60, 186, 88),
-            Color::Red => (219, 50, 54),
+            Color::Blue => (46, 83, 161),
+            Color::Green => (2, 170, 92),
+            Color::Red => (232, 66, 54),
         }
     }
 
